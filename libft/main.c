69c9f034b3b0772bdd8f18479cf194dbd345cc1c
dringkbar <nagito12396@gmail.com>
2020-12-23 14:18:26 +0900

    Add strlen

--- libft/main.c
+++ libft/main.c
@@ -6,7 +6,7 @@
 /*   By: riiringim <riiringim@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/22 12:39:28 by riiringim         #+#    #+#             */
-/*   Updated: 2020/12/22 23:07:02 by riiringim        ###   ########.fr       */
+/*   Updated: 2020/12/23 14:16:48 by riiringim        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,12 @@ void    *ft_memccpy(void *dst, const void *src, int c, size_t n);
 void    *ft_memmove(void *dst, const void *src, size_t len);
 void    *ft_memchr(const void *s, int c, size_t n);
 int     ft_memcmp(const void *s1, const void *s2, size_t n);
+size_t  ft_strlen(const char *s);
 
 int main(void)
 {
     char a[]="asdfqwer";
-    char b[] = "asdfqwer";
+    // char b[] = "asdfqwer";
 
   //printf("%s", ft_memset(a, 'c', sizeof(a)));
   //ft_bzero(a, sizeof(a));
@@ -32,5 +33,6 @@ int main(void)
   //printf("%s", ft_memccpy(b, a, 'p', sizeof(a)));
 //   printf("%s", ft_memmove(b, a, 4));
     // printf("%s", ft_memchr(a, 'q', sizeof(a)));
-    printf("%d", ft_memcmp(a,b,sizeof(a)));
+    // printf("%d", ft_memcmp(a,b,sizeof(a)));
+  printf("%zu", ft_strlen(a));
 }

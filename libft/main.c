f7d086b5beac2b7b9ef727abb9b75e714a90eb8e
dringkbar <nagito12396@gmail.com>
2020-12-23 16:07:07 +0900

    Add strlcpy

--- libft/main.c
+++ libft/main.c
@@ -6,26 +6,18 @@
 /*   By: riiringim <riiringim@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/22 12:39:28 by riiringim         #+#    #+#             */
-/*   Updated: 2020/12/23 14:16:48 by riiringim        ###   ########.fr       */
+/*   Updated: 2020/12/23 15:25:32 by riiringim        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
-
-void    *ft_memset(void *b, int c, size_t len);
-void    ft_bzero(void *s, size_t n);
-void    *ft_memcpy(void *dst, const void *src, size_t n);
-void    *ft_memccpy(void *dst, const void *src, int c, size_t n);
-void    *ft_memmove(void *dst, const void *src, size_t len);
-void    *ft_memchr(const void *s, int c, size_t n);
-int     ft_memcmp(const void *s1, const void *s2, size_t n);
-size_t  ft_strlen(const char *s);
+#include "libft.h"
 
 int main(void)
 {
     char a[]="asdfqwer";
-    // char b[] = "asdfqwer";
+    char b[] = "zxcvzxcv";
 
   //printf("%s", ft_memset(a, 'c', sizeof(a)));
   //ft_bzero(a, sizeof(a));
@@ -34,5 +26,6 @@ int main(void)
 //   printf("%s", ft_memmove(b, a, 4));
     // printf("%s", ft_memchr(a, 'q', sizeof(a)));
     // printf("%d", ft_memcmp(a,b,sizeof(a)));
-  printf("%zu", ft_strlen(a));
+  // printf("%zu", ft_strlen(a));
+  printf("%zu", ft_strlcpy(a,b,sizeof(b)));
 }
---
+++ libft/strlcpy.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   strlcpy.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: riiringim <riiringim@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/23 14:18:31 by riiringim         #+#    #+#             */
+/*   Updated: 2020/12/23 15:55:41 by riiringim        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdio.h>
+
+size_t ft_strlcpy(char *dst, const char *src, size_t dstsize)
+{
+    size_t i;
+
+    i = 0;
+    while (--dstsize && src[i])
+    {
+        dst[i] = src[i];
+        i++;
+    }
+    dst[i] = 0;
+    return (ft_strlen(src));
+}

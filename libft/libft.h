83d57f82b73e34c0c0ef1607cfdc9816e2b6451d
dringkbar <nagito12396@gmail.com>
2020-12-27 20:40:30 +0900

    Add ft_itoa

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: riiringim <riiringim@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/22 12:56:56 by riiringim         #+#    #+#             */
-/*   Updated: 2020/12/26 18:12:05 by riiringim        ###   ########.fr       */
+/*   Updated: 2020/12/27 18:18:21 by riiringim        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,5 +43,6 @@ char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 char 	**ft_split(char const *s, char c);
+char	*ft_itoa(int n);
 
 #endif

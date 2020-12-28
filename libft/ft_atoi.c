3db4a80e1747242850f4825534a7c78ee696d41a
dringkbar <nagito12396@gmail.com>
2020-12-28 20:59:05 +0900

    Edit atoi

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: riiringim <riiringim@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/24 23:26:11 by riiringim         #+#    #+#             */
-/*   Updated: 2020/12/28 19:48:16 by riiringim        ###   ########.fr       */
+/*   Updated: 2020/12/28 20:58:29 by riiringim        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 
 int	ft_atoi(const char *str)
 {
-	int res;
-	int sign;
-	int i;
+	long long	res;
+	int			sign;
+	int			i;
 
 	i = 0;
 	sign = 1;
@@ -32,7 +32,12 @@ int	ft_atoi(const char *str)
 	}
 	while (str[i] >= '0' && str[i] <= '9')
 	{
-		res = res * 10 + (str[i] - '0');
+		if (res * sign > 2147483647)
+			return (-1);
+		else if (res * sign < -2147483648)
+			return (0);
+		else
+			res = res * 10 + (str[i] - '0');
 		i++;
 	}
 	return (res * sign);

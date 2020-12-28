c8a67cc8b60d4944ef8b0949dfa546926917cadf
dringkbar <nagito12396@gmail.com>
2020-12-28 21:14:48 +0900

    Edit substr

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: riiringim <riiringim@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/25 16:37:59 by riiringim         #+#    #+#             */
-/*   Updated: 2020/12/28 19:49:31 by riiringim        ###   ########.fr       */
+/*   Updated: 2020/12/28 21:14:29 by riiringim        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,18 @@
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char *str;
-	size_t clen;
+	size_t i;
 
-	clen = len - start;
-	str = (char *)malloc(sizeof(char) * clen);
+	if (!s)
+		return (NULL);
+	str = (char *)malloc(sizeof(char) * len + 1);
+	if (ft_strlen(s) <= start)
+		return (ft_strdup(str));
 	if (!str)
 		return (NULL);
-	else
-		ft_strlcpy(str, &s[start], clen);
+	i = 0;
+	while (i < len)
+		str[i++] = s[start++];
+	str[i] = 0;
 	return (str);
 }

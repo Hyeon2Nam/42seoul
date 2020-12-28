e63eb048e4a417db1ce103b37497cda3b2d77a08
dringkbar <nagito12396@gmail.com>
2020-12-28 23:00:58 +0900

    Edit ft_putnbr

--- libft/ft_putnbr_fd.c
+++ libft/ft_putnbr_fd.c
@@ -6,15 +6,35 @@
 /*   By: riiringim <riiringim@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/28 16:29:16 by riiringim         #+#    #+#             */
-/*   Updated: 2020/12/28 19:48:57 by riiringim        ###   ########.fr       */
+/*   Updated: 2020/12/28 23:00:24 by riiringim        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_putnbr_fd(int n, int fd)
+void		ft_putnbr_fd(int nbr, int fd)
 {
-	char *s;
-	s = ft_itoa(n);
-	ft_putstr_fd(s, fd);
+	long	n;
+
+	n = nbr;
+	if (n == -2147483648)
+	{
+		ft_putstr_fd("-2147483648", fd);
+		return ;
+	}
+	if (n < 0)
+	{
+		ft_putchar_fd('-', fd);
+		ft_putnbr_fd(-n, fd);
+	}
+	else
+	{
+		if (n > 9)
+		{
+			ft_putnbr_fd(n / 10, fd);
+			ft_putnbr_fd(n % 10, fd);
+		}
+		else
+			ft_putchar_fd(n + '0', fd);
+	}
 }

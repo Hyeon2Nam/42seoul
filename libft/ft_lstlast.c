ee9ae207978f80fd6c2f56203f2e37e8c786a0c1
dringkbar <nagito12396@gmail.com>
2020-12-29 17:48:55 +0900

    Edit ft_lstsize and Add lstlast

--- libft/ft_lstlast.c
+++ libft/ft_lstlast.c
@@ -1,26 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstsize.c                                       :+:      :+:    :+:   */
+/*   ft_lstlast.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: riiringim <riiringim@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/12/29 17:13:42 by riiringim         #+#    #+#             */
-/*   Updated: 2020/12/29 17:47:45 by riiringim        ###   ########.fr       */
+/*   Created: 2020/12/29 17:24:47 by riiringim         #+#    #+#             */
+/*   Updated: 2020/12/29 17:43:34 by riiringim        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int ft_lstsize(t_list *lst)
+t_list	*ft_lstlast(t_list *lst)
 {
-	size_t i;
+	t_list *i;
 
-	i = 0;
-	while (lst)
-	{
-		lst = lst->next;
-		i++;
-	}
-	return (i);
+	if (!lst)
+		return (NULL);
+	i = ft_lstsize(lst);
+	return (i - 1);
 }
--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: riiringim <riiringim@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/22 12:56:56 by riiringim         #+#    #+#             */
-/*   Updated: 2020/12/29 17:14:43 by riiringim        ###   ########.fr       */
+/*   Updated: 2020/12/29 17:43:37 by riiringim        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,5 +61,6 @@ typedef struct	s_list
 t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
 int		ft_lstsize(t_list *lst);
+t_list	*ft_lstlast(t_list *lst);
 
 #endif

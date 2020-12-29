f06bab39b89ee0cfc12c095ceb1ff78b22b6576a
dringkbar <nagito12396@gmail.com>
2020-12-29 20:22:26 +0900

    Add ft_lstmap

---
+++ libft/ft_lstmap.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: riiringim <riiringim@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/29 19:55:27 by riiringim         #+#    #+#             */
+/*   Updated: 2020/12/29 20:22:05 by riiringim        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*new;
+	t_list	*content;
+
+	if (!lst || !f)
+		return (NULL);
+	content = ft_lstnew(f(lst->content));
+	if (!content)
+	{
+		ft_lstclear(&lst, del);
+		return (NULL);
+	}
+	new = content;
+	lst = lst->next;
+	while (lst)
+	{
+		content = ft_lstnew(f(lst->content));
+		if (!content)
+		{
+			ft_lstclear(&lst, del);
+			ft_lstclear(&new, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&new, content);
+		lst = lst->next;
+	}
+	return (new);
+}

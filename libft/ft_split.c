a8c87cb0340844654c1c8ccd19198c76c1958e55
dringkbar <nagito12396@gmail.com>
2021-01-05 14:02:27 +0900

    rename val

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: riiringim <riiringim@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/26 17:59:16 by riiringim         #+#    #+#             */
-/*   Updated: 2021/01/05 13:10:19 by riiringim        ###   ########.fr       */
+/*   Updated: 2021/01/05 13:59:45 by riiringim        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,29 +33,29 @@ static size_t	ft_cutnum(char const *s, char c)
 
 char			**ft_split(char const *s, char c)
 {
-	char	**ret;
-	char	*from;
-	size_t	index;
+	char	**str;
+	char	*start;
+	size_t	i;
 	size_t	size;
 
-	if (!(ret = (char**)malloc(sizeof(char*) * ft_cutnum(s, c) + 1)))
+	if (!(str = (char**)malloc(sizeof(char*) * ft_cutnum(s, c) + 1)))
 		return (0);
-	index = 0;
+	i = 0;
 	while (*s)
 	{
 		if (*s != c)
 		{
-			from = (char*)s;
+			start = (char*)s;
 			while (*s && *s != c)
 				++s;
-			size = s - from + 1;
-			if (!(ret[index] = (char*)malloc(size)))
+			size = s - start + 1;
+			if (!(str[i] = (char*)malloc(size)))
 				return (0);
-			ft_strlcpy(ret[index++], from, size);
+			ft_strlcpy(str[i++], start, size);
 		}
 		else
 			++s;
 	}
-	ret[index] = 0;
-	return (ret);
+	str[i] = 0;
+	return (str);
 }
